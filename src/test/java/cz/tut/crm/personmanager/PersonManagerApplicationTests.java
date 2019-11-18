d15edd57bacd0bf007bbdf9b5c0312763cede237
Jiri.Hermann <jiri.hermann@adastragrp.com>
2019-11-18 10:07:36 +0100

    first commit pm be

---
+++ src/test/java/cz/tut/crm/personmanager/PersonManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tut.crm.personmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
